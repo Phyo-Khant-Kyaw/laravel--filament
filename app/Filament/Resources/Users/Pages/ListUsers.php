682c45027f0cd3c5769ba10e3db7f4e8e97fb82f
Phyo-Khant-Kyaw <www.pkkcobra01@gmail.com>
2026-01-21 13:24:08 +0700

    first commit

---
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Users\Pages;
+
+use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table->columns([
+            TextColumn::make('name')->label('Name')->searchable()->sortable(),
+            TextColumn::make('email')->label('Email')->searchable()->sortable(),
+            IconColumn::make('email_verified_at')
+                ->label('Verified')
+                ->boolean(),
+        ]);
+    }
+}
